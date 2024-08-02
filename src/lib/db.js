d38fe0f1d598e83fdd25ca6532dc14d4faee6ff3
Leonel <viefmoon@gmail.com>
2024-08-02 16:55:31 -0600

    ddd

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,27 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
     dialect: 'mysql',
-    logging: false,
+    logging: false, // Desactivar logging en producción
 });
 
-async function connectDB() {
+const connectDB = async () => {
     try {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
+
+        // Sincronizar modelos con la base de datos
+        if (process.env.NODE_ENV === 'development') {
+            await sequelize.sync({ alter: true }); // Usar alter en desarrollo
+        } else {
+            await sequelize.sync(); // Sin alter en producción
+        }
+
+        console.log('All models were synchronized successfully.');
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
-}
+};
 
 module.exports = { sequelize, connectDB };
