d8685f99156179548fef5bbc1a771cf65039db1b
Leonel <viefmoon@gmail.com>
2024-08-05 11:25:01 -0600

    ff

---
+++ src/pages/api/get_customers.js
@@ -0,0 +1,38 @@
+const { connectDB } = require('../../lib/db');
+const Customer = require('../../models/Customer');
+const cors = require('cors');
+
+// Configurar CORS
+const corsMiddleware = cors({
+  origin: '*', // Permite todas las origenes en desarrollo. Ajusta esto en producción.
+  methods: ['GET'],
+});
+
+export default async function handler(req, res) {
+  // Aplicar el middleware CORS
+  await new Promise((resolve, reject) => {
+    corsMiddleware(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+
+  await connectDB();
+
+  if (req.method === 'GET') {
+    try {
+      // Obtener todos los clientes
+      const customers = await Customer.findAll();
+
+      res.status(200).json(customers);
+    } catch (error) {
+      console.error('Error al obtener los clientes:', error);
+      res.status(500).json({ error: 'Error al obtener los clientes' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
