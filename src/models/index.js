14682466fef32187a222b22bca47543697a9ae04
Leonel <viefmoon@gmail.com>
2024-08-08 13:02:04 -0600

    gg

--- src/models/index.js
+++ src/models/index.js
@@ -10,10 +10,12 @@ Item.belongsTo(Order, { foreignKey: 'orderId' });
 // Sincronizar todos los modelos con la base de datos
 const syncModels = async () => {
     try {
-        await sequelize.sync({ alter: true });  // Cambia esto de vuelta a { alter: true } después de la sincronización
-        console.log('La base de datos ha sido sincronizada.');
+        await Customer.sync({ alter: true });
+        await Item.sync({ alter: true });
+        await Order.sync({ alter: true });
+        console.log('Todos los modelos han sido sincronizados.');
     } catch (error) {
-        console.error('Error al sincronizar la base de datos:', error);
+        console.error('Error al sincronizar los modelos:', error);
     }
 };
 
