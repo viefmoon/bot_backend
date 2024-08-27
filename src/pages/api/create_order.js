ab2524f658bc01babc88ad9d81bda601b89c4140
Leonel <viefmoon@gmail.com>
2024-08-27 15:35:10 -0600

    jj

--- src/pages/api/create_order.js
+++ src/pages/api/create_order.js
@@ -713,7 +713,7 @@ async function calculateOrderItemsPrice(req, res) {
       if (item.selectedModifiers && item.selectedModifiers.length > 0) {
         const modifierTypes = await ModifierType.findAll({
           where: { productId: item.productId },
-          include: [Modifier],
+          include: [{ model: Modifier, as: "Modifiers" }],
         });
 
         for (const modifierType of modifierTypes) {
