fce5a0bea5d0dd59e0fceeb60bb544f63e8f7a52
Leonel <viefmoon@gmail.com>
2024-09-02 20:49:55 -0600

    dd

--- src/models/messageLog.js
+++ src/models/messageLog.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../config/database");
+const { sequelize } = require("../lib/db");
 
 const MessageLog = sequelize.define("MessageLog", {
   messageId: {
