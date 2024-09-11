386236db8d7abdb9f9ec54f78319146235b0a5e5
Leonel <viefmoon@gmail.com>
2024-09-11 10:36:47 -0600

    ffff

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -112,7 +112,7 @@ async function getMenuAvailability() {
 
     products.forEach((producto) => {
       const productoInfo = {
-        id: producto.id,
+        productId: producto.id,
         name: producto.name,
         active: producto.Availability?.available || false,
       };
@@ -120,7 +120,7 @@ async function getMenuAvailability() {
       // Agregar variantes
       if (producto.productVariants?.length > 0) {
         productoInfo.variantes = producto.productVariants.map((v) => ({
-          id: v.id,
+          variantId: v.id,
           name: v.name,
           active: v.Availability?.available || false,
         }));
@@ -131,7 +131,7 @@ async function getMenuAvailability() {
         productoInfo.modificadores = producto.modifierTypes.flatMap(
           (mt) =>
             mt.modifiers?.map((m) => ({
-              id: m.id,
+              modifierId: m.id,
               name: m.name,
               active: m.Availability?.available || false,
             })) || []
@@ -141,7 +141,7 @@ async function getMenuAvailability() {
       // Agregar ingredientes de pizza
       if (producto.pizzaIngredients?.length > 0) {
         productoInfo.ingredientesPizza = producto.pizzaIngredients.map((i) => ({
-          id: i.id,
+          pizzaIngredientId: i.id,
           name: i.name,
           active: i.Availability?.available || false,
         }));
