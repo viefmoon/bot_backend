0e0446181710bfcfb215f26fa84abbf2dc91dcec
Leonel <viefmoon@gmail.com>
2024-09-13 09:48:52 -0600

    dd

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -207,7 +207,7 @@ function extractMentionedProducts(message, menu) {
   for (const product of menu["Menu Disponible"]) {
     const productName = product.name.toLowerCase();
     let isProductMentioned = words.some(
-      (word) => word.length > 3 && partial_ratio(productName, word) > 90
+      (word) => word.length > 3 && partial_ratio(productName, word) > 70
     );
 
     // Buscar en las variantes
@@ -215,7 +215,7 @@ function extractMentionedProducts(message, menu) {
       words.some(
         (word) =>
           word.length > 3 &&
-          partial_ratio(variant.name.toLowerCase(), word) > 90
+          partial_ratio(variant.name.toLowerCase(), word) > 70
       )
     );
 
