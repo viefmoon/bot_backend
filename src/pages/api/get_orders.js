acd8d2c86b6a27ccc63864d41d6eb63668a8ed03
Leonel <viefmoon@gmail.com>
2024-09-24 12:02:36 -0600

    dd

--- src/pages/api/get_orders.js
+++ src/pages/api/get_orders.js
@@ -54,6 +54,7 @@ export default async function handler(req, res) {
               },
               {
                 model: SelectedModifier,
+                as: "selectedModifiers",
                 include: { model: Modifier },
               },
             ],
