c99e3981d7b094d0deea83f7a13d9855881140d9
Leonel <viefmoon@gmail.com>
2024-09-24 15:08:14 -0600

    dd

--- src/pages/api/get_orders_by_client.js
+++ src/pages/api/get_orders_by_client.js
@@ -1,4 +1,13 @@
-const { Order, OrderItem } = require("../../models");
+const {
+  Order,
+  OrderItem,
+  Product,
+  ProductVariant,
+  SelectedPizzaIngredient,
+  PizzaIngredient,
+  SelectedModifier,
+  Modifier,
+} = require("../../models");
 const cors = require("cors");
 
 // Configurar CORS
@@ -34,7 +43,47 @@ export default async function handler(req, res) {
           ["orderDate", "DESC"],
           ["dailyOrderNumber", "DESC"],
         ],
-        include: [{ model: OrderItem, as: "orderItems" }],
+        include: [
+          {
+            model: OrderItem,
+            as: "orderItems",
+            attributes: ["quantity", "price", "comments"],
+            include: [
+              { model: Product, attributes: ["name", "price"] },
+              { model: ProductVariant, attributes: ["name", "price"] },
+              {
+                model: SelectedPizzaIngredient,
+                as: "selectedPizzaIngredients",
+                attributes: ["half", "action"],
+                include: { model: PizzaIngredient, attributes: ["name"] },
+              },
+              {
+                model: SelectedModifier,
+                as: "selectedModifiers",
+                attributes: ["id"],
+                include: {
+                  model: Modifier,
+                  attributes: ["name", "price"],
+                },
+              },
+            ],
+          },
+        ],
+        attributes: [
+          "id",
+          "dailyOrderNumber",
+          "orderType",
+          "status",
+          "paymentStatus",
+          "deliveryInfo",
+          "totalCost",
+          "clientId",
+          "orderDate",
+          "estimatedTime",
+          "scheduledDeliveryTime",
+          "createdAt",
+          "updatedAt",
+        ],
       });
 
       res.status(200).json(orders);
