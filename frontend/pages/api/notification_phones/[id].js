edbb8ad3b6dc1835a5b104a3a97a6856c0b9de39
Leonel <viefmoon@gmail.com>
2024-10-02 11:32:58 -0600

    ff

--- frontend/pages/api/notification_phones/[id].js
+++ frontend/pages/api/notification_phones/[id].js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 export default async function handler(req, res) {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
