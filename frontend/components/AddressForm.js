9abd5b56c58b4514f25f06d005186c3a22d0097a
Leonel <viefmoon@gmail.com>
2024-10-03 11:34:33 -0600

    ddd

--- frontend/components/AddressForm.js
+++ frontend/components/AddressForm.js
@@ -58,12 +58,18 @@ export default function AddressForm({
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-1">
         {/* Campo de dirección completa */}
         <div className="col-span-1 sm:col-span-2">
-          <label
-            htmlFor="streetAddress"
-            className="block mb-0.5 text-xs font-medium text-gray-700"
-          >
-            {fieldTranslations.streetAddress}
-          </label>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="streetAddress"
+              className="block mb-0.5 text-xs font-medium text-gray-700"
+            >
+              {fieldTranslations.streetAddress}
+            </label>
+            <p className="text-xs text-red-600">
+              Importante: Incluya la orientación de la calle si aplica (ej.
+              Norte, Sur, etc.)
+            </p>
+          </div>
           <input
             type="text"
             id="streetAddress"
@@ -73,20 +79,21 @@ export default function AddressForm({
             className="w-full p-0.5 text-sm border rounded-md border-gray-300 focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
             required
           />
-          <p className="text-xs text-red-600 mt-0.5">
-            Importante: Incluya la orientación de la calle si aplica (ej. Norte,
-            Sur, etc.)
-          </p>
         </div>
 
         {/* Campo de detalles adicionales */}
         <div className="col-span-1 sm:col-span-2">
-          <label
-            htmlFor="additionalDetails"
-            className="block mb-0.5 text-xs font-medium text-gray-700"
-          >
-            Detalles adicionales
-          </label>
+          <div className="flex items-center justify-between">
+            <label
+              htmlFor="additionalDetails"
+              className="block mb-0.5 text-xs font-medium text-gray-700"
+            >
+              Detalles adicionales
+            </label>
+            <p className="text-xs text-gray-600">
+              Agregue información extra para ubicar su dirección más fácilmente
+            </p>
+          </div>
           <textarea
             id="additionalDetails"
             name="additionalDetails"
@@ -94,11 +101,8 @@ export default function AddressForm({
             onChange={handleChange}
             className="w-full p-0.5 text-sm border border-gray-300 rounded-md focus:ring-1 focus:ring-blue-500 focus:border-blue-500"
             rows="1"
+            placeholder="ej. entre calles, puntos de referencia"
           ></textarea>
-          <p className="text-xs text-gray-600 mt-0.5">
-            Agregue información extra para ubicar su dirección más fácilmente
-            (ej. entre calles, puntos de referencia)
-          </p>
         </div>
 
         {/* Campos agrupados */}
