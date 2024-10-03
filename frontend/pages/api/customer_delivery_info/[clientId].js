d80b8a6a638d6c8cbcf1e263ece948225cf58f4e
Leonel <viefmoon@gmail.com>
2024-10-03 13:10:33 -0600

    ff

--- frontend/pages/api/customer_delivery_info/[clientId].js
+++ frontend/pages/api/customer_delivery_info/[clientId].js
@@ -17,8 +17,23 @@ export default async function handler(req, res) {
           "Error al procesar la solicitud de obtención de información de entrega",
       });
     }
+  } else if (req.method === "PUT") {
+    try {
+      const updateDeliveryInfoResponse = await axios.put(
+        `${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/customer-delivery-info/${clientId}`,
+        req.body
+      );
+
+      res.status(200).json(updateDeliveryInfoResponse.data);
+    } catch (error) {
+      console.error("Error al actualizar la información de entrega:", error);
+      res.status(error.response?.status || 500).json({
+        error:
+          "Error al procesar la solicitud de actualización de información de entrega",
+      });
+    }
   } else {
-    res.setHeader("Allow", ["GET"]);
+    res.setHeader("Allow", ["GET", "PUT"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
