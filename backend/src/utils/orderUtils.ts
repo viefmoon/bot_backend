2861b35e696a51e2143a1f4d820b897edfae823f
Leonel <viefmoon@gmail.com>
2024-10-08 12:39:01 -0600

    ff

--- backend/src/utils/orderUtils.ts
+++ backend/src/utils/orderUtils.ts
@@ -1,25 +1,32 @@
 import { Op } from "sequelize";
 import { Order } from "../models";
+import moment from "moment-timezone";
 
 export async function getNextDailyOrderNumber(): Promise<number> {
-  // Obtener la fecha actual en la zona horaria de México
-  const mexicoTime = new Date().toLocaleString("en-US", { timeZone: "America/Mexico_City" });
-  const todayInMexico = new Date(mexicoTime);
-  todayInMexico.setHours(0, 0, 0, 0);
+  try {
+    // Obtener la fecha actual en la zona horaria de México
+    const todayInMexico = moment.tz("America/Mexico_City");
 
-  // Convertir el inicio y fin del día de México a UTC
-  const startOfDayUTC = new Date(todayInMexico.getTime() - todayInMexico.getTimezoneOffset() * 60000);
-  const endOfDayUTC = new Date(startOfDayUTC.getTime() + 24 * 60 * 60 * 1000);
+    // Definir el inicio y fin del día en UTC
+    const startOfDayUTC = todayInMexico.clone().startOf("day").utc();
+    const endOfDayUTC = todayInMexico.clone().endOf("day").utc();
 
-  const lastOrder = await Order.findOne({
-    where: {
-      createdAt: {
-        [Op.gte]: startOfDayUTC,
-        [Op.lt]: endOfDayUTC,
+    const lastOrder = await Order.findOne({
+      where: {
+        createdAt: {
+          [Op.gte]: startOfDayUTC.toDate(),
+          [Op.lt]: endOfDayUTC.toDate(),
+        },
       },
-    },
-    order: [["dailyOrderNumber", "DESC"]],
-  });
+      order: [["dailyOrderNumber", "DESC"]],
+    });
 
-  return lastOrder ? lastOrder.dailyOrderNumber + 1 : 1;
+    return lastOrder ? lastOrder.dailyOrderNumber + 1 : 1;
+  } catch (error) {
+    console.error(
+      "Error al obtener el siguiente número de orden diaria:",
+      error
+    );
+    throw error;
+  }
 }
