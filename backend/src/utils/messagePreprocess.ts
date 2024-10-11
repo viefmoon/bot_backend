613acf93e62486e79ba6431655a464c182f0168a
leo <viefmoon@gmail.com>
2024-10-10 22:21:38 -0600

    ffff

--- backend/src/utils/messagePreprocess.ts
+++ backend/src/utils/messagePreprocess.ts
@@ -225,6 +225,8 @@ function mapSynonym(normalizedWord: string): string | null {
     bbq: ["barbacoa", "barbicue"],
     picosas: ["picositas"],
     alitas: ["alas"],
+    gajos: ["gajos"],
+    mixtas: ["mixtas"],
     extra: ["adicional", "mas", "extras", "doble"],
     doradas: ["doraditas"],
     queso: ["gratinadas", "gratinado", "quesos"],
