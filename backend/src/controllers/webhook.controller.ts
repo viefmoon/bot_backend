3258e0681f630cd7ba9a0700ef1b48182eab69d8
Leonel <viefmoon@gmail.com>
2024-10-14 09:32:51 -0600

    gg

--- backend/src/controllers/webhook.controller.ts
+++ backend/src/controllers/webhook.controller.ts
@@ -14,6 +14,7 @@ export class WebhookController {
   @Post()
   async handleWebhook(@Req() req: Request, @Res() res: Response) {
     console.log("req.headers", req.headers);
+    console.log("stripe-signature", req.headers["stripe-signature"]);
     console.log("req.body", req.body);
     if (req.headers["stripe-signature"]) {
       return this.webhookService.handleStripeWebhook(req, res);
