c506174e0320a5f2f971c7d7ca464ec22ed12ca5
Leonel <viefmoon@gmail.com>
2024-10-15 09:39:39 -0600

    fff

--- backend/src/handlers/orderHandlers.ts
+++ backend/src/handlers/orderHandlers.ts
@@ -425,7 +425,7 @@ export async function handleOrderModification(
       const filteredItem: any = {
         quantity: item.quantity,
         productId: item.productId,
-        productVariantId: item.productVariant.id,
+        productVariantId: item.productVariantId,
       };
 
       if (item.comments) filteredItem.comments = item.comments;
