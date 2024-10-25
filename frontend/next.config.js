27d31b074bba62168baba21ae48777ac70b8a7bc
Leonel <viefmoon@gmail.com>
2024-10-25 15:05:38 -0600

    fff

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  server: {
-    host: "127.0.0.1", // Forzar IPv4
-    port: 3000,
-  },
+module.exports = {
+  reactStrictMode: false,
 };
-
-module.exports = nextConfig;
