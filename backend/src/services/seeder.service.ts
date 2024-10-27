c80ed6496d321a81af902ce7fba8bdd58d8b1ef6
Leonel <viefmoon@gmail.com>
2024-10-27 13:27:56 -0600

    fff

--- backend/src/services/seeder.service.ts
+++ backend/src/services/seeder.service.ts
@@ -1,14 +1,10 @@
 import { Injectable, OnModuleInit } from "@nestjs/common";
 import { seedMenuItems } from "../../seeders/seedMenuItems";
-import SeederControl from "../models/seederControl";
 
 @Injectable()
 export class SeederService implements OnModuleInit {
   async onModuleInit() {
     try {
-      // Asegúrate de que la tabla SeederControl existe
-      await SeederControl.sync();
-      
       // Ejecuta el seeder
       await seedMenuItems();
     } catch (error) {
