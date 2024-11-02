8818a8ff86323b6dc78e629fc47ae510fb800699
Leonel <viefmoon@gmail.com>
2024-11-02 13:40:30 -0600

    gg

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,7 +19,10 @@ async function bootstrap() {
   app.use(
     json({
       verify: (req: any, res, buf) => {
-        req.rawBody = buf;
+        if (req.url.includes("/webhook")) {
+          // Solo para rutas de webhook
+          req.rawBody = buf.toString();
+        }
       },
     })
   );
