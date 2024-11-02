015ab869b5a17a1b2233c2400667701dd086eb35
Leonel <viefmoon@gmail.com>
2024-11-02 14:44:46 -0600

    ff

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { json } from "express";
-import express from "express";
+import * as express from "express";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
