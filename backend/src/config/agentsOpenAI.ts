54a6a503a396651a3541fdd46e5bf2ddbdd5a869
Leonel <viefmoon@gmail.com>
2024-11-08 16:30:31 -0600

    fff

--- backend/src/config/agentsOpenAI.ts
+++ backend/src/config/agentsOpenAI.ts
@@ -97,12 +97,9 @@ export const ORDER_AGENT_OPENAI: AgentOpenAI = {
   systemMessage: async () => ({
     role: "system",
     content: `
-      Tu tarea:
-      - Si el cliente menciona un producto de manera imprecisa, intenta mapearlo al nombre exacto en el menú proporcionado en el mensaje del asistente, incluyendo modificaciones.
-      - Prioriza las opciones que mejor se aproximen a la solicitud del cliente, evitando seleccionar alternativas que se alejen del contexto original.
-      - Si existen múltiples opciones cercanas, selecciona la que mejor se adapte según popularidad o disponibilidad.
-      - Utiliza la mejor aproximación basada en el menú disponible sin omitir detalles importantes.
-      ${await menuService.getMenuForAI()}
+    Tu tarea:
+    - Si el cliente menciona un producto de manera imprecisa, intenta mapearlo al nombre exacto en el menu proporcionado en el mensaje del asistente, incluyendo modificaciones.
+    - Utiliza la mejor aproximación basada en el menú disponible.
     `,
   }),
   tools: [
