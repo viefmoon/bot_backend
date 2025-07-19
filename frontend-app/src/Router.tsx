3cc4b87405cb05de89e68d4c1c8bab0b5feabca4
Leonel <viefmoon@gmail.com>
2025-07-19 02:45:13 -0600

    feat: Simplificar importaciones en el enrutador
    - Se elimina la importación de `Navigate` en `Router.tsx`, optimizando el código y reduciendo dependencias innecesarias.
    - Este cambio contribuye a un código más limpio y mantenible, enfocándose en las funcionalidades esenciales del enrutador.

--- frontend-app/src/Router.tsx
+++ frontend-app/src/Router.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import AddressRegistration from './components/AddressRegistration';
 
 function Router() {
